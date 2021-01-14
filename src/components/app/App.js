fc21daef23b89799d9bcad0eee255e5d4aa444ca
jmanit <jainmanit18@gmail.com>
2021-01-14 15:54:09 +0530

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from 'semantic-ui-react';
+
+import MainFormContainer from '../main-form'
+
+const App =()=> 
+<Container textAlign='center'>
+<h1>Multi Step Form with Semantic UI</h1>
+<MainFormContainer />
+</Container>
+
+export default App 
